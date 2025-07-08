de8bf74746d05007f4ef84177b73a1377e93166f
GItAlyon <alyona070106@gmail.com>
2025-07-08 19:50:01 +0700

    App.vue

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+  <EcosystemSimulation />
+</template>
+
+<script>
+import EcosystemSimulation from './components/EcosystemSimulation.vue'
+
+export default {
+  name: 'App',
+  components: {
+    EcosystemSimulation
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 20px;
+  font-family: Arial, sans-serif;
+}
+</style>
